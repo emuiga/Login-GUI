f72c14792e05a8ba46d1f8f66cdcca08136b0699
Steve <muigastephen14@gmail.com>
2022-08-09 10:01:15 +0300

    gui

--- GUI.java
+++ GUI.java
@@ -1,7 +1,12 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
 
-public class GUI {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+public class GUI implements ActionListener, java.awt.event.ActionListener {
 
     public GUI() {
 
@@ -10,11 +15,43 @@ public class GUI {
     
         JPanel panel =new JPanel();
         JFrame frame = new JFrame();
-        frame.setSize(200, 200);
+        frame.setSize(350, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
         frame.add(panel);
         
-        panel.setLayout
+        panel.setLayout(null);
 
-}
+        JLabel userLabel = new JLabel("User");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        JTextField userText = new JTextField();
+        userText.setBounds(100, 20, 165, 25);
+        panel.add(userText);
+
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwordLabel);
+
+        JPasswordField passwordText = new JPasswordField();
+        passwordText.setBounds(100, 50, 165, 25);
+        panel.add(passwordText);
+
+        JButton button = new JButton("Login");
+        button.setBounds(10, 80, 80, 25);
+        button.addActionListener(new GUI());
+        panel.add(button);
+
+        JLabel  success = new JLabel();
+        success.setBounds(10, 110, 300, 25);
+        panel.add(success);
+        success.setText
+
+        frame.setVisible(true);
+        }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    }
