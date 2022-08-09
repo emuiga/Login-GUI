c06f57389551e91dea23f9dcc9457cb903a689a8
Steve <muigastephen14@gmail.com>
2022-08-09 09:01:15 +0300

    gui

---
+++ GUI.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI {
+
+    public GUI() {
+
+    }
+    public static void main(String[] args) {
+    
+        JPanel panel =new JPanel();
+        JFrame frame = new JFrame();
+        frame.setSize(200, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(panel);
+        
+        panel.setLayout
+
+}
