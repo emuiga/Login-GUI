a4cfa127f7671d7c23700153274c217fec6dd1fa
Steve <muigastephen14@gmail.com>
2022-08-10 09:25:57 +0300

    gui

--- GUI.java
+++ GUI.java
@@ -8,50 +8,63 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 public class GUI implements ActionListener, java.awt.event.ActionListener {
 
-    public GUI() {
+       private static JLabel userLabel;
+       private static JPanel panel;
+       private static JFrame frame;
+       private static JTextField userText;
+       private static JPasswordField passwordText;
+       private static JButton button;
+       private static JLabel passwordLabel;
+       private static JLabel success;
 
-    }
+
     public static void main(String[] args) {
     
-        JPanel panel =new JPanel();
-        JFrame frame = new JFrame();
+        panel =new JPanel();
+        frame = new JFrame();
         frame.setSize(350, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
         
         panel.setLayout(null);
 
-        JLabel userLabel = new JLabel("User");
+        userLabel = new JLabel("User");
         userLabel.setBounds(10, 20, 80, 25);
         panel.add(userLabel);
 
-        JTextField userText = new JTextField();
+        userText = new JTextField();
         userText.setBounds(100, 20, 165, 25);
         panel.add(userText);
 
-        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel = new JLabel("Password");
         passwordLabel.setBounds(10, 50, 80, 25);
         panel.add(passwordLabel);
 
-        JPasswordField passwordText = new JPasswordField();
+        passwordText = new JPasswordField();
         passwordText.setBounds(100, 50, 165, 25);
         panel.add(passwordText);
 
-        JButton button = new JButton("Login");
+        button = new JButton("Login");
         button.setBounds(10, 80, 80, 25);
         button.addActionListener(new GUI());
         panel.add(button);
 
-        JLabel  success = new JLabel();
+        success = new JLabel();
         success.setBounds(10, 110, 300, 25);
         panel.add(success);
-        success.setText
 
         frame.setVisible(true);
         }
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        
+        String user = userText.getText();
+        String password = passwordText.getText();      //deprecated  
+        System.out.println(user + ", " + password);
+
+        if(user.equals("Steve") && password.equals("guesshowmuchilovefood123")) {
+            success.setText("Login successful!");
+        } else {
+            success.setText("Retry");
+        }
     }
     }
